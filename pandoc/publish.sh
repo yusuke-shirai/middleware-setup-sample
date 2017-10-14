f84984a47ebea3789252cdcf7f67a8b80fed1c84
白井　悠介 <shiraiyusuke@shiroi-yuusuke-no-iMac.local>
2017-10-15 05:47:48 +0900

    temporary

--- pandoc/publish.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-pushd src
-pandoc -s $1.md -o ../dist/$1.pdf  --table-of-contents --toc-depth=3 --number-sections --latex-engine=lualatex  --data-dir=. -V documentclass=ltjsarticle --include-in-header=header.latex
-pandoc -s $1.md -o ../dist/$1.html --table-of-contents --toc-depth=3 --number-sections --self-contained -t html5
-popd
